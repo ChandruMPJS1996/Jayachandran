b9096d5c6461ba76ef20502da4d27084c3157238
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-02-08 16:14:06 +0530

    Create oddno.c

---
+++ oddno.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the numbers(intervals):");
+scanf("%d%d",&a,&b);
+printf("the numbers between %d and %d are:",a,b);
+if(a%2==0)
+{
+printf("odd number");
+}
+else
+{
+printf("not")
+}
+}
