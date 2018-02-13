00c4c5bba6f118a174d790cbaf65d97814a37340
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-02-13 16:27:43 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int mins,hour=0;
+  clrscr();
+	scanf("%d",&mins);
+ 	if(mins<60)
+	{
+   printf("%d %d",hour,mins);
+  	}
+ 	else 
+	{
+	    hour=mins/60;
+	    mins=mins%60;
+	    printf("%d %d",hour,mins);
+	}
+	return 0;
+  getch();
+}
