32ac05631918e50cae5e1b3aa3a11e6ba230dde5
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-02-13 16:10:17 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+(void) 
+{
+int a,b,lcm,gcd;
+clrscr();
+scanf("%d %d",&a,&b);
+for(int i=2;i<=a&&i<=b;i++)
+{
+	if(a%i==0 && b%i==0)
+	{
+		gcd=i;
+	}
+}
+lcm=(a*b)/gcd;
+printf("%d",lcm);
+return 0;
+getch();
+}
