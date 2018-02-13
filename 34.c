1d05b7f44ffef90ca6289fb333cb09b0ddf8d9c0
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-02-13 16:33:52 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[30];
+int i=0,count=0;
+clrscr();
+  printf("enter the string");
+  scanf("%s",&str);
+while(str[i]!='\0')
+
+{
+
+if(str[i]=="\n
+{
+++count;
+}
+++i;
+}
+count++;
+printf("no of lines is %d ",count);
+}
+getch();
+}
