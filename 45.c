a7db4fd07b94021f5c2e3ceffc4cc037c1334e93
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-03-01 15:34:56 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+  clrscr();
+	int i,count=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+  getch();
+}
