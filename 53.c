4c9198702649d1b2dcbdbdd130cd3e62f53f3dee
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-03-13 15:52:33 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5],i;
+clrscr();
+printf("enter the value");
+scanf("%d",&a);
+for(i=0;i<=5;i++);
+{
+sum=sum+i;
+}
+printf("the value of sum=%d");
+getch();
+}
