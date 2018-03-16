4ac49bb18fb502ca7c7369cbcb415c0e74006315
ChandruMPJS1996 <35652520+ChandruMPJS1996@users.noreply.github.com>
2018-03-16 14:08:31 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+  clrscr();
+	scanf("%d %d",&i,&j);
+	i=i^j;
+	k=i^j;
+	i=i^j;
+	printf("%d %d",i,j);
+	return 0;
+  getch();
+  }
